480014c8570bd203c8101d64ed4452399368b7a2
Peter Svensson <peter@sparetimecoders.com>
2022-10-21 16:51:08 +0200

    chore: updated with more repos

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "example" {
   template_owner      = var.github_owner
   template_repository = module.template.template_repo
 }
-#
+
 module "terraform-aws-aurora-mysql" {
   source = "./tf-repo"
 
@@ -45,54 +45,119 @@ module "terraform-aws-dns-validated-certificate" {
   name        = "terraform-aws-dns-validated-certificate"
 }
 
+module "terraform-aws-elasticache-redis" {
+  source = "./tf-repo"
+
+  description = ""
+  name        = "terraform-aws-elasticache-redis"
+}
+
+module "terraform-aws-k8s" {
+  source = "./tf-repo"
+
+  description = "Module for creating Kubernetes clusters using kOps"
+  name        = "terraform-aws-k8s"
+}
+
+module "terraform-aws-k8s-addons-argocd" {
+  source = "./tf-repo"
+
+  description = ""
+  name        = "terraform-aws-k8s-addons-argocd"
+}
+
+module "terraform-aws-k8s-addons-cluster-autoscaler" {
+  source = "./tf-repo"
+
+  description = ""
+  name        = "terraform-aws-k8s-addons-cluster-autoscaler"
+}
+
+module "terraform-aws-k8s-addons-external-dns" {
+  source = "./tf-repo"
+
+  description = ""
+  name        = "terraform-aws-k8s-addons-external-dns"
+}
+
+module "terraform-aws-k8s-addons-external-secrets-operator" {
+  source = "./tf-repo"
+
+  description = ""
+  name        = "terraform-aws-k8s-addons-external-secrets-operator"
+}
+
+module "terraform-aws-k8s-addons-fluentbit" {
+  source = "./tf-repo"
+
+  description = ""
+  name        = "terraform-aws-k8s-addons-fluentbit"
+}
+
+module "terraform-aws-k8s-addons-github-runners" {
+  source = "./tf-repo"
+
+  description = ""
+  name        = "terraform-aws-k8s-addons-github-runners"
+}
+
+module "terraform-aws-k8s-addons-grafana-agent-operator" {
+  source = "./tf-repo"
+
+  description = ""
+  name        = "terraform-aws-k8s-addons-grafana-agent-operator"
+}
+
+module "terraform-aws-k8s-argocd-cluster-secret" {
+  source = "./tf-repo"
+
+  description = ""
+  name        = "terraform-aws-k8s-argocd-cluster-secret"
+}
+
+module "terraform-aws-k8s-network" {
+  source = "./tf-repo"
+
+  description = ""
+  name        = "terraform-aws-k8s-network"
+}
+
+module "terraform-aws-kops-state-store" {
+  source = "./tf-repo"
+
+  description = ""
+  name        = "terraform-aws-kops-state-store"
+}
+
+module "terraform-aws-rds-instance-mysql" {
+  source = "./tf-repo"
+
+  description = ""
+  name        = "terraform-aws-rds-instance-mysql"
+}
+
+module "terraform-aws-rds-instance-postgresql" {
+  source = "./tf-repo"
+
+  description = ""
+  name        = "terraform-aws-rds-instance-postgresql"
+}
+
+module "terraform-cloudamqp-rabbitmq" {
+  source = "./tf-repo"
+
+  description = ""
+  name        = "terraform-cloudamqp-rabbitmq"
+}
+
+
 #
-#module "terraform-aws-k8s-network" {
-#  source = "./tf-repo"
-#
-#  description = ""
-#  name        = "terraform-aws-k8s-network"
-#}
-#
-#module "terraform-cloudamqp-rabbitmq" {
-#  source = "./tf-repo"
-#
-#  description = ""
-#  name        = "terraform-cloudamqp-rabbitmq"
-#}
-#
-#module "terraform-aws-k8s" {
-#  source = "./tf-repo"
-#
-#  description = ""
-#  name        = "terraform-aws-k8s"
-#}
-#
-#module "terraform-aws-k8s-addons-cluster-autoscaler" {
-#  source = "./tf-repo"
-#
-#  description = ""
-#  name        = "terraform-aws-k8s-addons-cluster-autoscaler"
-#}
 #
 #module "nodeamqp" {
 #  source = "./tf-repo"
 #
 #  description = ""
 #  name        = "nodeamqp"
-#}
-#
-#module "terraform-aws-k8s-addons-external-secrets-operator" {
-#  source = "./tf-repo"
-#
-#  description = ""
-#  name        = "terraform-aws-k8s-addons-external-secrets-operator"
-#}
-#
-#module "terraform-aws-k8s-addons-grafana-agent-operator" {
-#  source = "./tf-repo"
-#
-#  description = ""
-#  name        = "terraform-aws-k8s-addons-grafana-agent-operator"
 #}
 #
 #module "govulncheck-action" {
@@ -123,47 +188,6 @@ module "terraform-aws-dns-validated-certificate" {
 #  name        = "terraform-aws-k8s-addons-external-secrets"
 #}
 #
-#module "terraform-aws-k8s-addons-fluentbit" {
-#  source = "./tf-repo"
-#
-#  description = ""
-#  name        = "terraform-aws-k8s-addons-fluentbit"
-#}
-#
-#module "terraform-aws-elasticache-redis" {
-#  source = "./tf-repo"
-#
-#  description = ""
-#  name        = "terraform-aws-elasticache-redis"
-#}
-#
-#module "terraform-aws-rds-instance-mysql" {
-#  source = "./tf-repo"
-#
-#  description = ""
-#  name        = "terraform-aws-rds-instance-mysql"
-#}
-#
-#module "terraform-aws-rds-instance-postgresql" {
-#  source = "./tf-repo"
-#
-#  description = ""
-#  name        = "terraform-aws-rds-instance-postgresql"
-#}
-#
-#module "terraform-aws-k8s-argocd-cluster-secret" {
-#  source = "./tf-repo"
-#
-#  description = ""
-#  name        = "terraform-aws-k8s-argocd-cluster-secret"
-#}
-#
-#module "terraform-aws-kops-state-store" {
-#  source = "./tf-repo"
-#
-#  description = ""
-#  name        = "terraform-aws-kops-state-store"
-#}
 #
 #module "terraform-aws-k8s-aws-lb-role-fix" {
 #  source = "./tf-repo"
@@ -172,26 +196,8 @@ module "terraform-aws-dns-validated-certificate" {
 #  name        = "terraform-aws-k8s-aws-lb-role-fix"
 #}
 #
-#module "terraform-aws-k8s-addons-external-dns" {
-#  source = "./tf-repo"
 #
-#  description = ""
-#  name        = "terraform-aws-k8s-addons-external-dns"
-#}
 #
-#module "terraform-aws-k8s-addons-github-runners" {
-#  source = "./tf-repo"
-#
-#  description = ""
-#  name        = "terraform-aws-k8s-addons-github-runners"
-#}
-#
-#module "terraform-aws-k8s-addons-argocd" {
-#  source = "./tf-repo"
-#
-#  description = ""
-#  name        = "terraform-aws-k8s-addons-argocd"
-#}
 #
 #module "argocd-bootstrap-template" {
 #  source = "./tf-repo"
